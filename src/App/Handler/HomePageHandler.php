e8373290c6961098cbf13be7a80a9f19473a99b8
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-11 16:24:16 +0300

    changed HomeHundler and HomehandlerFactory

--- src/App/Handler/HomePageHandler.php
+++ src/App/Handler/HomePageHandler.php
@@ -22,79 +22,13 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class HomePageHandler implements RequestHandlerInterface
 {
-    /** @var string */
-    private $containerName;
-
-    /** @var Router\RouterInterface */
-    private $router;
-
-    /** @var null|TemplateRendererInterface */
-    private $template;
-
-    public function __construct(
-        string $containerName,
-        Router\RouterInterface $router,
-        ?TemplateRendererInterface $template = null
-    ) {
-        $this->containerName = $containerName;
-        $this->router        = $router;
-        $this->template      = $template;
-    }
-
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $data = [];
-
-        switch ($this->containerName) {
-            case PimpleContainer::class:
-                $data['containerName'] = 'Pimple';
-                $data['containerDocs'] = 'https://pimple.symfony.com/';
-                break;
-            case ServiceManager::class:
-                $data['containerName'] = 'Laminas Servicemanager';
-                $data['containerDocs'] = 'https://docs.laminas.dev/laminas-servicemanager/';
-                break;
-            case ContainerBuilder::class:
-                $data['containerName'] = 'Symfony DI Container';
-                $data['containerDocs'] = 'https://symfony.com/doc/current/service_container.html';
-                break;
-            case 'Elie\PHPDI\Config\ContainerWrapper':
-            case PHPDIContainer::class:
-                $data['containerName'] = 'PHP-DI';
-                $data['containerDocs'] = 'http://php-di.org';
-                break;
-            case MinimalContainer::class:
-                $data['containerName'] = 'Chubbyphp Container';
-                $data['containerDocs'] = 'https://github.com/chubbyphp/chubbyphp-container';
-                break;
-        }
-
-        if ($this->router instanceof Router\FastRouteRouter) {
-            $data['routerName'] = 'FastRoute';
-            $data['routerDocs'] = 'https://github.com/nikic/FastRoute';
-        } elseif ($this->router instanceof Router\LaminasRouter) {
-            $data['routerName'] = 'Laminas Router';
-            $data['routerDocs'] = 'https://docs.laminas.dev/laminas-router/';
-        }
-
-        if ($this->template === null) {
-            return new JsonResponse([
-                'welcome' => 'Congratulations! You have installed the mezzio skeleton application.',
-                'docsUrl' => 'https://docs.mezzio.dev/mezzio/',
-            ] + $data);
-        }
-
-        if ($this->template instanceof PlatesRenderer) {
-            $data['templateName'] = 'Plates';
-            $data['templateDocs'] = 'http://platesphp.com/';
-        } elseif ($this->template instanceof TwigRenderer) {
-            $data['templateName'] = 'Twig';
-            $data['templateDocs'] = 'http://twig.sensiolabs.org/documentation';
-        } elseif ($this->template instanceof LaminasViewRenderer) {
-            $data['templateName'] = 'Laminas View';
-            $data['templateDocs'] = 'https://docs.laminas.dev/laminas-view/';
-        }
-
-        return new HtmlResponse($this->template->render('app::home-page', $data));
+        $target = $request->getQueryParams()['target'] ?? 'World';
+        $target = htmlspecialchars($target, ENT_HTML5, 'UTF-8');
+        return new HtmlResponse(sprintf(
+            '<h1>Hello %s</h1>',
+            $target
+        ));
     }
 }
--- src/App/Handler/HomePageHandlerFactory.php
+++ src/App/Handler/HomePageHandlerFactory.php
@@ -16,14 +16,6 @@ class HomePageHandlerFactory
 {
     public function __invoke(ContainerInterface $container): RequestHandlerInterface
     {
-        $router = $container->get(RouterInterface::class);
-        assert($router instanceof RouterInterface);
-
-        $template = $container->has(TemplateRendererInterface::class)
-            ? $container->get(TemplateRendererInterface::class)
-            : null;
-        assert($template instanceof TemplateRendererInterface || null === $template);
-
-        return new HomePageHandler(get_class($container), $router, $template);
+        return new HomePageHandler();
     }
 }
