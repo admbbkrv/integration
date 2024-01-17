547392990fb2f4e78d73de41f377e5451f8a2aa2
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-17 12:38:58 +0300

    Написал функционал для получения Access Token

--- src/AmoApiClient/src/Handler/GetApiTokenHandler.php
+++ src/AmoApiClient/src/Handler/GetApiTokenHandler.php
@@ -4,31 +4,35 @@ declare(strict_types=1);
 
 namespace AmoApiClient\Handler;
 
+use AmoCRM\Client\AmoCRMApiClient;
+use Laminas\Diactoros\Response\RedirectResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Laminas\Diactoros\Response\HtmlResponse;
-use Mezzio\Template\TemplateRendererInterface;
 
 class GetApiTokenHandler implements RequestHandlerInterface
 {
     /**
-     * @var TemplateRendererInterface
+     * Клиент для работы с API
+     * @var AmoCRMApiClient
      */
-    private $renderer;
+    private AmoCRMApiClient $apiClient;
 
-    public function __construct(TemplateRendererInterface $renderer)
+    public function __construct(AmoCRMApiClient $apiClient)
     {
-        $this->renderer = $renderer;
+        $this->apiClient = $apiClient;
     }
 
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Do some work...
-        // Render and return a response:
-        return new HtmlResponse($this->renderer->render(
-            'amo-api-client::get-api-token',
-            [] // parameters to pass to template
-        ));
+        $state = bin2hex(random_bytes(16));
+
+        //Генерации ссылки с параметрами на страницу авторизации
+        $authorizationUrl = $this->apiClient->getOAuthClient()->getAuthorizeUrl([
+            'state' => $state,
+            'mode' => 'post_message', //post_message - редирект произойдет в открытом окне, popup - редирект произойдет в окне родителе
+        ]);
+
+        return new RedirectResponse($authorizationUrl);
     }
 }
--- src/AmoApiClient/src/Handler/GetApiTokenHandlerFactory.php
+++ src/AmoApiClient/src/Handler/GetApiTokenHandlerFactory.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace AmoApiClient\Handler;
 
-use Mezzio\Template\TemplateRendererInterface;
+use AmoCRM\Client\AmoCRMApiClient;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Dotenv\Dotenv;
 
 class GetApiTokenHandlerFactory
 {
     public function __invoke(ContainerInterface $container) : GetApiTokenHandler
     {
-        return new GetApiTokenHandler($container->get(TemplateRendererInterface::class));
+        //Загрузка переменных окружения из .env
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../../../../.env');
+
+        $clientId = $_ENV['CLIENT_ID'];
+        $clientSecret = $_ENV['CLIENT_SECRET'];
+        $redirectUri = $_ENV['CLIENT_REDIRECT_URI'];
+
+        //Запускаем клиент API библиотеки
+        $apiClient = new AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+
+        return new GetApiTokenHandler($apiClient);
     }
 }
