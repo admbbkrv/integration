cec511070ce3a2e8c1f5c1f19afa1e6b5beea442
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-19 15:40:10 +0300

    Добавил класс для хранения констант

---
+++ src/AmoApiClient/src/Constants/AmoApiConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AmoApiClient\Constants;
+
+/**
+ * Класс хранит константы для работы API клинета
+ */
+class AmoApiConstants
+{
+    public const TOKEN_FILE = DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR . 'token_info.json';
+}
