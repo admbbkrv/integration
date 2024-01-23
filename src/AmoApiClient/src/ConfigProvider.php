6ff54511a6cf4daf72208dfaba85271253383590
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-23 12:18:47 +0300

    Настроил config DI контейнера

--- src/AmoApiClient/src/ConfigProvider.php
+++ src/AmoApiClient/src/ConfigProvider.php
@@ -5,19 +5,27 @@ declare(strict_types=1);
 namespace AmoApiClient;
 
 
-use AmoApiClient\Factory\AmoApiClientWithTokenFactory;
-use AmoApiClient\Factory\AmoCRMApiClientFactory;
-use AmoApiClient\Handler\ApiMainHandler;
-use AmoApiClient\Handler\ApiMainHandlerFactory;
+use AmoApiClient\Handler\AuthAmoHandler;
+use AmoApiClient\Handler\AuthAmoHandlerFactory;
+use AmoApiClient\Handler\MainApiHandler;
+use AmoApiClient\Handler\MainApiHandlerFactory;
+use AmoApiClient\Handler\RedirectUriApiHandler;
+use AmoApiClient\Handler\RedirectUriApiHandlerFactory;
 use AmoApiClient\Middleware\ApiAmoAuthMiddleware;
 use AmoApiClient\Middleware\ApiAmoAuthMiddlewareFactory;
+use AmoApiClient\Middleware\DotEnvMiddleware;
+use AmoApiClient\Middleware\DotEnvMiddlewareFactory;
 use AmoApiClient\Services\AccessTokenService\AccessTokenService;
 use AmoApiClient\Services\AccessTokenService\AccessTokenServiceFactory;
 use AmoApiClient\Services\AccessTokenService\GetTokenInterface;
 use AmoApiClient\Services\AccessTokenService\GetTokenService;
 use AmoApiClient\Services\AccessTokenService\SaveTokenInterface;
 use AmoApiClient\Services\AccessTokenService\SaveTokenService;
-use AmoCRM\Client\AmoCRMApiClient;
+use AmoApiClient\Services\OAuth\OAuthConfig;
+use AmoApiClient\Services\OAuth\OAuthService;
+use AmoCRM\OAuth\OAuthConfigInterface;
+use AmoCRM\OAuth\OAuthServiceInterface;
+use Symfony\Component\Dotenv\Dotenv;
 
 /**
  * The configuration provider for the AmoApiClient module
@@ -49,13 +57,17 @@ class ConfigProvider
             'invokables' => [
                 SaveTokenInterface::class => SaveTokenService::class,
                 GetTokenInterface::class => GetTokenService::class,
+                OAuthServiceInterface::class => OAuthService::class,
+                OAuthConfigInterface::class => OAuthConfig::class,
+                Dotenv::class => Dotenv::class,
             ],
             'factories'  => [
-                AmoCRMApiClient::class => AmoCRMApiClientFactory::class,
-                'AmoClientWithToken' => AmoApiClientWithTokenFactory::class,
-                ApiMainHandler::class => ApiMainHandlerFactory::class,
+                MainApiHandler::class => MainApiHandlerFactory::class,
                 AccessTokenService::class => AccessTokenServiceFactory::class,
                 ApiAmoAuthMiddleware::class => ApiAmoAuthMiddlewareFactory::class,
+                AuthAmoHandler::class => AuthAmoHandlerFactory::class,
+                RedirectUriApiHandler::class => RedirectUriApiHandlerFactory::class,
+                DotEnvMiddleware::class => DotEnvMiddlewareFactory::class,
             ],
         ];
     }
