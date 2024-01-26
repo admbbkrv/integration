5e6ed0bf9b691df54c2819340b524d1c47c57cca
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-26 17:23:06 +0300

    Добавил интерфейс обработки данных котакта для импорта и его реализацию

---
+++ src/UnisenderApi/src/Services/PrepareForImportService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UnisenderApi\Services;
+
+use AmoCRM\Collections\ContactsCollection;
+
+/**
+ * Сервис для подготовки данных контактов для импорта в Unisender
+ */
+class PrepareForImportService implements PrepareForImportInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function prepare(ContactsCollection $contactsCollection): array
+    {
+        $data = [];
+        foreach ($contactsCollection as $contactModel) {
+            $name = $contactModel->getName();
+            $emails = $contactModel->getCustomFieldsValues()->getBy('fieldCode', 'EMAIL')->getValues();
+
+            foreach ($emails as $email) {
+                $data[] = [
+                    $email->getValue(),
+                    $name,
+                ];
+            }
+        }
+
+        $preparedContacts = [
+            'field_names' => [
+                'email',
+                'Name',
+            ],
+            'data' => $data,
+        ];
+
+        return $preparedContacts;
+    }
+}
