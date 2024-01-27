f8f9ee0665a0b5cb40d2ecb8556b61b68dc72f5b
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-27 12:45:23 +0300

    Доработал логику фильтрации контактов по email. Исключил случаи, когда у контакта нет никаких доп полей

--- src/AmoApiClient/src/Services/ContactServices/FilterWithEmailService.php
+++ src/AmoApiClient/src/Services/ContactServices/FilterWithEmailService.php
@@ -15,19 +15,25 @@ class FilterWithEmailService implements FilterWithEmailInterface
     /**
      * @inheritDoc
      */
-    public function filterWithEmail(ContactsCollection $contactsCollection): ContactsCollection
-    {
+    public function filterWithEmail(
+        ContactsCollection $contactsCollection
+    ): ContactsCollection {
         foreach ($contactsCollection as $contactModel) {
 
-            $email_fields = $contactModel->getCustomFieldsValues()->getBy('fieldCode', 'EMAIL');
-
-            if (!$email_fields) {
-                $contactsCollection->removeBy(
-                    'id',
-                    $contactModel->getId()
-                );
+            if (
+                $contactModel->getCustomFieldsValues() !== null
+                && $contactModel->getCustomFieldsValues()
+                    ->getBy('fieldCode', 'EMAIL') !== null
+            ) {
+                continue;
             }
+
+            $contactsCollection->removeBy(
+                'id',
+                $contactModel->getId()
+            );
         }
+
         return $contactsCollection;
     }
 }
