4943130ed6674f3a991352dc52bcdd0eb382ea0e
aabubakarov <aabubakarov@team.amocrm.com>
2024-01-27 12:46:34 +0300

    Переработал логику импорта контактов, так как может приходить 500+ контактов

--- src/UnisenderApi/src/Services/ImportContactsService.php
+++ src/UnisenderApi/src/Services/ImportContactsService.php
@@ -14,9 +14,23 @@ class ImportContactsService implements ImportContactsInterface
     /**
      * @inheritDoc
      */
-    public function importContacts(array $contacts, UnisenderApi $unisenderApi): array
-    {
-        $responce = $unisenderApi->importContacts($contacts);
+    public function importContacts(
+        array $contacts,
+        UnisenderApi $unisenderApi
+    ): array {
+        if (count($contacts) > 500) {
+
+            $chunks = array_chunk($contacts, 500);
+
+            $responce = [];
+
+            foreach ($chunks as $chunk) {
+                $responce[] = $unisenderApi->importContacts($chunk);
+            }
+
+        } else {
+            $responce = $unisenderApi->importContacts($contacts);
+        }
 
         return json_decode($responce, true);
     }
