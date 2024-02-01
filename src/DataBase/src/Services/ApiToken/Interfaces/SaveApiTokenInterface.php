02f75a0040abc7f7c7ee201aa8959a74b78acf88
aabubakarov <aabubakarov@team.amocrm.com>
2024-02-01 18:47:23 +0300

    Интерфейс и сервис для сохранения ApiToken в БД

---
+++ src/DataBase/src/Services/ApiToken/Interfaces/SaveApiTokenInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DataBase\Services\ApiToken\Interfaces;
+
+use DataBase\Models\ApiToken;
+
+/**
+ * Интерфейс сохранения данных токенов в БД
+ */
+interface SaveApiTokenInterface
+{
+    /**
+     * Сохранение ApiToken в БД
+     * @param string $accessToken
+     * @param int $expires
+     * @param string $refreshToken
+     * @param string $baseDomain
+     * @param int|null $userId
+     * @param string|null $apiKey
+     * @return mixed
+     */
+    public function save(
+        string $accessToken,
+        int $expires,
+        string $refreshToken,
+        string $baseDomain,
+        ?int $userId = null,
+        ?string $apiKey = null
+    ): ApiToken;
+}
---
+++ src/DataBase/src/Services/ApiToken/SaveApiTokenService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DataBase\Services\ApiToken;
+
+use DataBase\Models\ApiToken;
+use DataBase\Services\ApiToken\Interfaces\SaveApiTokenInterface;
+
+/**
+ * Сервис сохранения данных токенов в БД
+ */
+class SaveApiTokenService implements SaveApiTokenInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function save(
+        string $accessToken,
+        int $expires,
+        string $refreshToken,
+        string $baseDomain,
+        ?int $userId = null,
+        ?string $apiKey = null
+    ): ApiToken {
+        $values = [
+            'access_token' => $accessToken,
+            'expires' => $expires,
+            'refresh_token' => $refreshToken,
+            'base_domain' => $baseDomain,
+        ];
+
+        if ($userId !== null) {
+            $values['user_id'] = $userId;
+        }
+
+        if ($apiKey !== null) {
+            $values['api_key'] = $apiKey;
+        }
+
+        return ApiToken::query()->updateOrCreate(
+            [
+                'base_domain' => $baseDomain,
+            ],
+            $values,
+        );
+    }
+}
