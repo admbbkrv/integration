2b37a7612a036669a0c8b3ae432df7da6abe5815
aabubakarov <aabubakarov@team.amocrm.com>
2024-02-02 15:19:56 +0300

    Переработал работу генерации ссылки на авторизацию

--- src/AmoApiClient/src/Handler/AuthApiHandler.php
+++ src/AmoApiClient/src/Handler/AuthApiHandler.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace AmoApiClient\Handler;
 
 use AmoApiClient\Services\AmoClient\Interfaces\GetAmoCRMApiClientInterface;
+use DataBase\Services\Integration\Get\Interfaces\GetIntegrationInterface;
+use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\Response\RedirectResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -16,11 +18,14 @@ use Psr\Http\Server\RequestHandlerInterface;
 class AuthApiHandler implements RequestHandlerInterface
 {
     private GetAmoCRMApiClientInterface $getAmoCRMApiClient;
+    private GetIntegrationInterface $getIntegration;
 
     public function __construct(
+        GetIntegrationInterface $getIntegration,
         GetAmoCRMApiClientInterface $getAmoCRMApiClient
     ) {
         $this->getAmoCRMApiClient = $getAmoCRMApiClient;
+        $this->getIntegration = $getIntegration;
     }
 
     public function handle(
@@ -31,7 +36,44 @@ class AuthApiHandler implements RequestHandlerInterface
             session_start();
         }
 
-        $integration_id = $request->getQueryParams()['integration_id'];
+        $postParams = $request->getParsedBody();
+
+        if (!$postParams['client_id'] && !$postParams['client_secret']) {
+            $message = 'You have not passed the client_id or client_secret';
+            $response = [
+                'Error' => [
+                    'Message' => $message
+                ],
+            ];
+
+            return new JsonResponse($response);
+        }
+
+        $integration = $this->getIntegration
+            ->get('client_id', $postParams['client_id']);
+
+        if ($integration === null) {
+            $message = 'Your integration is not registered. Please register your integration.';
+            $response = [
+                'Error' => [
+                    'Message' => $message
+                ],
+            ];
+
+            return new JsonResponse($response);
+        }
+
+        if ($integration->client_secret != $postParams['client_secret']) {
+            $message = 'You entered an invalid client_secret';
+            $response = [
+                'Error' => [
+                    'Message' => $message
+                ],
+            ];
+
+            return new JsonResponse($response);
+        }
+        $integration_id = $integration->id;
 
         $state = bin2hex(random_bytes(16));
 
