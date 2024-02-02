bbd702a553857a4ba510ee8ee3c7721fa62161aa
aabubakarov <aabubakarov@team.amocrm.com>
2024-02-02 17:55:57 +0300

    Добавил конфиги для di контейнера

--- src/DataBase/src/ConfigProvider.php
+++ src/DataBase/src/ConfigProvider.php
@@ -8,18 +8,32 @@ use DataBase\Handler\CreateIntegrationHandler;
 use DataBase\Handler\CreateIntegrationHandlerFactory;
 use DataBase\Handler\CreateUserHandler;
 use DataBase\Handler\CreateUserHandlerFactory;
+use DataBase\Handler\SaveApiKeyHandler;
+use DataBase\Handler\SaveApiKeyHandlerFactory;
 use DataBase\Handler\SaveIntegrationHandler;
 use DataBase\Handler\SaveIntegrationHandlerFactory;
 use DataBase\Middleware\DoConnectToDBMiddleware;
 use DataBase\Middleware\DoConnectToDBMiddlewareFactory;
+use DataBase\Services\ApiToken\create\Interfaces\SaveAccessTokenInterface;
+use DataBase\Services\ApiToken\create\Interfaces\SaveApiKeyInterface;
+use DataBase\Services\ApiToken\create\Interfaces\SaveApiTokenInterface;
+use DataBase\Services\ApiToken\create\SaveAccessTokenService;
+use DataBase\Services\ApiToken\create\SaveApiKeyService;
+use DataBase\Services\ApiToken\create\SaveApiTokenService;
+use DataBase\Services\ApiToken\get\GetAccessTokenService;
+use DataBase\Services\ApiToken\get\GetApiTokenService;
+use DataBase\Services\ApiToken\get\Interfaces\GetAccessTokenInterface;
+use DataBase\Services\ApiToken\get\Interfaces\GetApiTokenInterface;
 use DataBase\Services\ConnectToMysqlDBService;
-use DataBase\Services\CreateUserService;
 use DataBase\Services\Integration\Create\Interfaces\SaveIntegrationInterface;
 use DataBase\Services\Integration\Create\SaveIntegrationService;
 use DataBase\Services\Integration\Get\GetIntegrationService;
 use DataBase\Services\Integration\Get\Interfaces\GetIntegrationInterface;
 use DataBase\Services\Interfaces\ConnectToDBInterface;
-use DataBase\Services\Interfaces\User\CreateUserInterface;
+use DataBase\Services\User\create\Interfaces\SaveUserInterface;
+use DataBase\Services\User\create\SaveUserService;
+use DataBase\Services\User\get\GetUserService;
+use DataBase\Services\User\get\Interfaces\GetUserInterface;
 
 /**
  * The configuration provider for the DataBase module
@@ -48,19 +62,30 @@ class ConfigProvider
     public function getDependencies() : array
     {
         return [
-            'invokables' => [
+            'invokables' => array(
                 ConnectToDBInterface::class => ConnectToMysqlDBService::class,
-                CreateUserInterface::class => CreateUserService::class,
+                //User
+                SaveUserInterface::class => SaveUserService::class,
+                GetUserInterface::class => GetUserService::class,
                 //Integration
                 SaveIntegrationInterface::class => SaveIntegrationService::class,
                 GetIntegrationInterface::class => GetIntegrationService::class,
-            ],
+                //ApiToken
+                SaveApiTokenInterface::class => SaveApiTokenService::class,
+                SaveAccessTokenInterface::class => SaveAccessTokenService::class,
+                SaveApiKeyInterface::class => SaveApiKeyService::class,
+                GetApiTokenInterface::class => GetApiTokenService::class,
+                GetAccessTokenInterface::class => GetAccessTokenService::class,
+            ),
             'factories'  => [
-                CreateUserHandler::class => CreateUserHandlerFactory::class,
                 DoConnectToDBMiddleware::class => DoConnectToDBMiddlewareFactory::class,
+                //User
+                CreateUserHandler::class => CreateUserHandlerFactory::class,
                 //Integration
                 CreateIntegrationHandler::class => CreateIntegrationHandlerFactory::class,
                 SaveIntegrationHandler::class => SaveIntegrationHandlerFactory::class,
+                //ApiToken
+                SaveApiKeyHandler::class => SaveApiKeyHandlerFactory::class,
             ],
         ];
     }
