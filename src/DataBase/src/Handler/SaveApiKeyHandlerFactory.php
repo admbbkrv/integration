c1af9ba8450c0a5003fb95469b8edbcd7ff74d2c
aabubakarov <aabubakarov@team.amocrm.com>
2024-02-05 19:23:13 +0300

    Сохранение api ключа, создание вебхука, первичная синхронизация

--- src/DataBase/src/Handler/SaveApiKeyHandlerFactory.php
+++ src/DataBase/src/Handler/SaveApiKeyHandlerFactory.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace DataBase\Handler;
 
+use AmoApiClient\Services\AmoClient\Interfaces\GetAmoCRMApiClientInterface;
+use AmoApiClient\Services\AmoClient\Webhooks\Interfaces\GenerateWebhookModelInterface;
+use AmoApiClient\Services\ContactServices\Interfaces\FilterWithEmailInterface;
+use AmoApiClient\Services\ContactServices\Interfaces\GetAllContactsInterface;
 use DataBase\Services\ApiToken\create\Interfaces\SaveApiKeyInterface;
+use DataBase\Services\ApiToken\get\Interfaces\GetAccessTokenInterface;
 use DataBase\Services\User\get\Interfaces\GetUserInterface;
+use Mezzio\Router\RouterInterface;
 use Psr\Container\ContainerInterface;
+use UnisenderApi\Services\ImportContactsInterface;
+use UnisenderApi\Services\PrepareForImportInterface;
+use UnisenderApi\Services\UnisenderApi\Interfaces\GetUnisenderApiInterface;
 
 /**
  * Фабрика генерации SaveApiKeyHandler обработчика
@@ -18,6 +27,15 @@ class SaveApiKeyHandlerFactory
         return new SaveApiKeyHandler(
             $container->get(GetUserInterface::class),
             $container->get(SaveApiKeyInterface::class),
+            $container->get(GetAmoCRMApiClientInterface::class),
+            $container->get(RouterInterface::class),
+            $container->get(GenerateWebhookModelInterface::class),
+            $container->get(GetAccessTokenInterface::class),
+            $container->get(GetUnisenderApiInterface::class),
+            $container->get(GetAllContactsInterface::class),
+            $container->get(FilterWithEmailInterface::class),
+            $container->get(PrepareForImportInterface::class),
+            $container->get(ImportContactsInterface::class)
         );
     }
 }
