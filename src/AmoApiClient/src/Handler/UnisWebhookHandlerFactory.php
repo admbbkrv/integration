736c77323bdf597eb86444f0f466e9b4c266e69b
aabubakarov <aabubakarov@team.amocrm.com>
2024-02-07 14:20:08 +0300

    Обработчик вебхуков

--- src/AmoApiClient/src/Handler/UnisWebhookHandlerFactory.php
+++ src/AmoApiClient/src/Handler/UnisWebhookHandlerFactory.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace AmoApiClient\Handler;
 
-use DataBase\Services\ApiToken\get\GetApiTokenService;
-use DataBase\Services\Contact\create\Interfaces\SaveContactInterface;
-use DataBase\Services\User\get\Interfaces\GetUserInterface;
+use Beanstalkd\Producers\WebhookProducer;
 use Psr\Container\ContainerInterface;
-use UnisenderApi\Services\PrepareData\GetPreparedContactsForAddService;
-use UnisenderApi\Services\PrepareData\GetPreparedContactsForDeleteService;
-use UnisenderApi\Services\PrepareData\GetPreparedContactsForUpdateService;
-use UnisenderApi\Services\UnisenderApi\Interfaces\GetUnisenderApiInterface;
 
 /**
  * Фабрика генерации UnisWebhookHandler обработчика
@@ -21,13 +15,7 @@ class UnisWebhookHandlerFactory
     public function __invoke(ContainerInterface $container) : UnisWebhookHandler
     {
         return new UnisWebhookHandler(
-            $container->get(GetPreparedContactsForAddService::class),
-            $container->get(GetPreparedContactsForDeleteService::class),
-            $container->get(GetPreparedContactsForUpdateService::class),
-            $container->get(GetApiTokenService::class),
-            $container->get(GetUnisenderApiInterface::class),
-            $container->get(SaveContactInterface::class),
-            $container->get(GetUserInterface::class)
+            $container->get(WebhookProducer::class),
         );
     }
 }
